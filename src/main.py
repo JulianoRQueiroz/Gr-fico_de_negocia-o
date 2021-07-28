95600c4a560aae26bab908d796d5dad23aa97e01
JulianoQueiroz <juliano.invest@outlook.com>
2021-07-28 17:02:53 -0300

    Verificando gráfico das médias moveis das empresas

--- src/main.py
+++ src/main.py
@@ -5,14 +5,14 @@ import pandas_datareader as web
 
 plt.style.use("dark_background")
 
-ma_1 = 30 
+ma_1 = 30
 ma_2 = 100 
 
 start = dt.datetime.now() - dt.timedelta(days=365 * 3) # Uma duração expressando a diferença entre dois
 end = dt.datetime.now()
 
 ''' pacote que nos permite criar um objeto DataFrame pandas usando várias fontes de dados da internet'''
-data = web.DataReader('FB', 'yahoo', start, end) 
+data = web.DataReader('AAPL', 'yahoo', start, end) # FB / AAPL / TSLA
 # SMA = Média Móvel Simples - Uma média móvel simples é formada pelo cálculo do preço médio de um ativo num número específico de períodos
 data[f'SMA_{ma_1}'] = data['Adj Close'].rolling(window=ma_1).mean() # rolling() fornece uma janela contínua para operações matemáticas
 data[f'SMA_{ma_2}'] = data['Adj Close'].rolling(window=ma_2).mean() # mean() media de uma lista 
